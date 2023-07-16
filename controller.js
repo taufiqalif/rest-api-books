f1819a5fd0e6d96f9f0e475b085b70adeb481e0d
taufiqalif <aliftaufiq4@gmail.com>
2023-07-16 16:28:27 +0700

    berhasil menampilkan semua data

--- controller.js
+++ controller.js
@@ -8,3 +8,15 @@ var connection = require('./koneksi');
 exports.index = function(req, res){
   response.ok("rest api berjalan", res)
 }
+
+// tampikan semua data dari database
+
+exports.views = function(req, res){
+  connection.query('SELECT * FROM books', function(error, rows, fileds){
+    if(error){
+      connection.log(error);
+    }else {
+      response.ok(rows, res);
+    }
+  });
+};
